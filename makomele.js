9c57106c5ce241c226c67496a33ceb027d165d30
macmele <macmelejames@gmail.com>
2024-08-02 01:29:09 +0300

    Updated changes 2

--- makomele.js
+++ makomele.js
@@ -17,7 +17,7 @@
      const sidem = document.querySelector(".sidemenu");
      const Makomele=document.querySelector(".fa-bars");
      const gete=document.querySelector(".fa-times");
-     Makomele.addEventListener("click",()=>{
+     Makomele?.addEventListener("click",()=>{
         sidem.classList.add("show");
         
      
@@ -26,11 +26,43 @@
      }
      
     )
-    gete.addEventListener("click",()=>{
+    gete?.addEventListener("click",()=>{
         sidem.classList.remove("show");
     }
     )
 
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const textElement = document.querySelector(".Text");
+        const originalText = textElement.textContent;
+        let index = originalText.length;
+        let deleting = true;
+    
+        function deleteText() {
+            if (index >= 0) {
+                textElement.textContent = originalText.slice(0, index);
+                index--;
+                setTimeout(deleteText, 20); // Adjust the speed of deletion here (100ms per character)
+            } else {
+                deleting = false;
+                setTimeout(addText, 500); // Pause before starting to add text
+            }
+        }
+    
+        function addText() {
+            if (index < originalText.length) {
+                textElement.textContent = originalText.slice(0, index + 1);
+                index++;
+                setTimeout(addText, 20); // Adjust the speed of adding text here (100ms per character)
+            } else {
+                deleting = true;
+                setTimeout(deleteText, 500); // Pause before starting to delete text again
+            }
+        }
+    
+        deleteText();
+    });
+    
     
 
 
